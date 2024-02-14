c5debddc9d13ead9eae171c3d2df97749a673e35
roxiiii <59148760+roxiiii@users.noreply.github.com>
2024-02-14 03:24:14 +0200

    Add files via upload

--- more.js
+++ more.js
@@ -21,7 +21,8 @@ var quotes = [
     'I can\'t wait to see you again',
     'I\'m lucky I met you',
     'You\'re my favourite notification',
-    'I\'m proud of you'
+    'I\'m proud of you',
+    'Always remember, my heart holds you when my arms cannot.',
 ]
 
 function newQuote()
@@ -30,4 +31,21 @@ function newQuote()
     document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
 }
 
+const buttons = document.querySelectorAll("[data-carousel-button]")
 
+buttons.forEach(button => {
+button.addEventListener("click", () => {
+const offset = button.dataset.carouselButton === "next" ? 1 : -1
+const slides = button
+.closest("[data-carousel]")
+.querySelector("[data-slides]")
+
+const activeSlide = slides.querySelector("[data-active]")
+let newIndex = [...slides.children].indexOf(activeSlide) + offset
+if (newIndex < 0) newIndex = slides.children.length - 1
+if (newIndex >= slides.children.length) newIndex = 0
+
+slides.children[newIndex].dataset.active = true
+delete activeSlide.dataset.active
+})
+})
